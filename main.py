936f519e40757734d5a5633e2e95edfc3d1f7284
sahil69-create <sgamer4448@gmail.com>
2026-03-16 16:13:35 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Hello from Python server!")
